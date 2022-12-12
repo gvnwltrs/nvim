f0b0ccd3642231297817ddd889867c4cd1db632a
Gavin <waltersg298@gmail.com>
2022-12-12 07:19:53 -0700

    adding basic nvim conifg

---
+++ init.lua
@@ -0,0 +1,147 @@
+-- Defaults 
+--local api = vim.api
+--local g = vim.g
+--local opt = vim.opt
+--local cmd = vim.cmd
+vim.opt.mouse = "a"
+vim.opt.clipboard = 'unnamedplus'
+vim.o.termguicolors = true
+vim.o.syntax = 'on'
+vim.o.errorbells = false
+vim.o.smartcase = true
+vim.o.showmode = false
+vim.bo.swapfile = false
+vim.o.backup = false
+vim.o.undodir = vim.fn.stdpath('config') .. '/undodir'
+vim.o.undofile = true
+vim.o.incsearch = true
+vim.o.hidden = true
+vim.o.completeopt='menuone,noinsert,noselect'
+vim.bo.autoindent = true
+vim.bo.smartindent = true
+vim.o.tabstop = 2
+vim.o.softtabstop = 2
+vim.o.shiftwidth = 2
+vim.o.expandtab = true
+vim.wo.number = true
+vim.wo.relativenumber = true
+vim.wo.signcolumn = 'yes'
+vim.wo.wrap = false
+
+-- Simple Key Bindings 
+vim.g.mapleader = " "
+
+vim.api.nvim_set_keymap('i', 'jk', '<ESC>', {noremap = true})
+
+vim.o.relativenumber = true
+
+-- Packer Plugin Setup
+local vim = vim
+local execute = vim.api.nvim_command
+local fn = vim.fn
+-- ensure that packer is installed
+local install_path = fn.stdpath('data')..'/site/pack/packer/opt/packer.nvim'
+if fn.empty(fn.glob(install_path)) > 0 then
+    execute('!git clone https://github.com/wbthomason/packer.nvim '..install_path)
+    execute 'packadd packer.nvim'
+end
+vim.cmd('packadd packer.nvim')
+local packer = require'packer'
+local util = require'packer.util'
+packer.init({
+  package_root = util.join_paths(vim.fn.stdpath('data'), 'site', 'pack')
+})
+
+--- Configure Packer Plugins 
+packer.startup(function()
+  local use = use
+
+  -- add you plugins here like:
+  -- use 'neovim/nvim-lspconfig'
+
+  --- Dev Icons 
+  use 'nvim-tree/nvim-web-devicons'
+  require'nvim-web-devicons'.setup {
+ -- your personnal icons can go here (to override)
+ -- you can specify color or cterm_color instead of specifying both of them
+ -- DevIcon will be appended to `name`
+ override = {
+  zsh = {
+    icon = "",
+    color = "#428850",
+    cterm_color = "65",
+    name = "Zsh"
+  }
+ };
+ -- globally enable different highlight colors per icon (default to true)
+ -- if set to false all icons will have the default icon's color
+ color_icons = true;
+ -- globally enable default icons (default to false)
+ -- will get overriden by `get_icons` option
+ default = true;
+}
+  
+  --- Treesitter
+  use {'nvim-treesitter/nvim-treesitter'}
+
+  -- Status Line
+  use {
+    'nvim-lualine/lualine.nvim',                     -- statusline
+    requires = {'kyazdani42/nvim-web-devicons',
+                opt = true}
+  }
+  --require('lualine').setup()
+  require('lualine').setup{
+    options = {
+      theme = 'dracula'
+    }
+  }
+
+  --- NVIM Tree
+  use {
+    'nvim-tree/nvim-tree.lua',
+    requires = {
+      'nvim-tree/nvim-web-devicons', -- optional, for file icons
+    },
+    tag = 'nightly' -- optional, updated every week. (see issue #1193)
+  }
+
+  -- disable netrw at the very start of your init.lua (strongly advised)
+  vim.g.loaded_netrw = 1
+  vim.g.loaded_netrwPlugin = 1
+
+  -- set termguicolors to enable highlight groups
+  vim.opt.termguicolors = true
+
+  -- empty setup using defaults
+  require("nvim-tree").setup()
+
+  -- OR setup with some options
+  require("nvim-tree").setup({
+    open_on_setup = true,
+    sort_by = "case_sensitive",
+    view = {
+      side = "left",
+      width = 30,
+      adaptive_size = true,
+      mappings = {
+        list = {
+          { key = "u", action = "dir_up" },
+        },
+      },
+    },
+    renderer = {
+      group_empty = true,
+    },
+    filters = {
+      dotfiles = true,
+    },
+  })
+
+
+  -- Dracula 
+
+
+
+  end
+)
