7f7533086ea87d2d6cd4bb21f943b0f4e336398d
Gavin <waltersg298@gmail.com>
2022-12-15 16:46:00 -0700

    updated config

--- init.lua
+++ init.lua
@@ -30,10 +30,24 @@ vim.wo.wrap = false
 
 -- Simple Key Bindings 
 vim.g.mapleader = " "
+--vim.g.mapleader = ","
 
 vim.api.nvim_set_keymap('i', 'jk', '<ESC>', {noremap = true})
+--vim.api.nvim_set_keymap('n', 'm', ':NvimTreeToggle<CR>', {noremap = true})
+vim.api.nvim_set_keymap('n', '<F6>', ':NvimTreeToggle<CR>', {noremap = true})
+--vim.api.nvim_set_keymap('n', '<C-l>', ':NvimTreeToggle<CR>', {noremap = true})
+vim.api.nvim_set_keymap('n', '<C-l>', '<C-w>l', {noremap = true})
+vim.api.nvim_set_keymap('n', '<C-h>', '<C-w>h', {noremap = true})
+vim.api.nvim_set_keymap('n', '<C-j>', '<C-w>j', {noremap = true})
+vim.api.nvim_set_keymap('n', '<C-k>', '<C-w>k', {noremap = true})
+vim.api.nvim_set_keymap('n', '<C-space>', ':bnext<CR>', {noremap = true})
+vim.api.nvim_set_keymap('n', '<C-b>', ':BufDel<CR>', {noremap = true})
+--vim.api.nvim_set_keymap('n', '<C-Tab>', ':BufferLineCycleNext<CR>', {noremap = true})
 
 vim.o.relativenumber = false
+
+-- Colorscheme
+vim.cmd[[colorscheme tokyonight]]
 
 -- Packer Plugin Setup
 local vim = vim
@@ -59,27 +73,30 @@ packer.startup(function()
   -- add you plugins here like:
   -- use 'neovim/nvim-lspconfig'
 
+  -- Colorschemes 
+  use 'folke/tokyonight.nvim'
+
   --- Dev Icons 
   use 'nvim-tree/nvim-web-devicons'
   require'nvim-web-devicons'.setup {
- -- your personnal icons can go here (to override)
- -- you can specify color or cterm_color instead of specifying both of them
- -- DevIcon will be appended to `name`
- override = {
-  zsh = {
-    icon = "",
-    color = "#428850",
-    cterm_color = "65",
-    name = "Zsh"
+    -- your personnal icons can go here (to override)
+    -- you can specify color or cterm_color instead of specifying both of them
+    -- DevIcon will be appended to `name`
+    override = {
+      zsh = {
+        icon = "",
+       color = "#428850",
+       cterm_color = "65",
+       name = "Zsh"
+      }
+    };
+    -- globally enable different highlight colors per icon (default to true)
+    -- if set to false all icons will have the default icon's color
+    color_icons = true;
+    -- globally enable default icons (default to false)
+    -- will get overriden by `get_icons` option
+    default = true;
   }
- };
- -- globally enable different highlight colors per icon (default to true)
- -- if set to false all icons will have the default icon's color
- color_icons = true;
- -- globally enable default icons (default to false)
- -- will get overriden by `get_icons` option
- default = true;
-}
   
   --- Treesitter
   use {'nvim-treesitter/nvim-treesitter'}
@@ -114,7 +131,7 @@ packer.startup(function()
   vim.opt.termguicolors = true
 
   -- empty setup using defaults
-  require("nvim-tree").setup()
+  --require("nvim-tree").setup()
 
   -- OR setup with some options
   require("nvim-tree").setup({
@@ -127,6 +144,7 @@ packer.startup(function()
       mappings = {
         list = {
           { key = "u", action = "dir_up" },
+          { key = "<C-shift>m", action = ":NvimTreeToggle<CR>" },
         },
       },
     },
@@ -138,10 +156,69 @@ packer.startup(function()
     },
   })
 
+  -- Bufferline 
+  -- using packer.nvim
+  use {'akinsho/bufferline.nvim', tag = "v3.*", requires = 'nvim-tree/nvim-web-devicons'}
+  require("bufferline").setup{}
 
-  -- Dracula 
 
+  -- Which-key 
+  use {
+  "folke/which-key.nvim",
+  config = function()
+      require("which-key").setup {
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+      }
+    end
+  }
 
+  -- Telescope
+  use {
+    'nvim-telescope/telescope.nvim', tag = '0.1.0',
+    -- or                            , branch = '0.1.x',
+    requires = { {'nvim-lua/plenary.nvim'} }
+  }
+  require('telescope').setup{
+    defaults = {
+      -- Default configuration for telescope goes here:
+      -- config_key = value,
+      mappings = {
+        i = {
+          -- map actions.which_key to <C-h> (default: <C-/>)
+          -- actions.which_key shows the mappings for your picker,
+          -- e.g. git_{create, delete, ...}_branch for the git_branches picker
+          ["<C-h>"] = "which_key"
+        }
+      }
+    },
+    pickers = {
+      -- Default configuration for builtin pickers goes here:
+      -- picker_name = {
+      --   picker_config_key = value,
+      --   ...
+      -- }
+      -- Now the picker_config_key will be applied every time you call this
+      -- builtin picker
+    },
+    extensions = {
+      -- Your extension configuration goes here:
+      -- extension_name = {
+      --   extension_config_key = value,
+      -- }
+      -- please take a look at the readme of the extension you want to configure
+    }
+  }
+
+  -- Bufdel
+  use {'ojroques/nvim-bufdel'}
+  require('bufdel').setup {
+    next = 'tabs',  -- or 'cycle, 'alternate'
+    quit = true,    -- quit Neovim when last buffer is closed
+  }
+
+  -- DO NOT ADD ANYTHING BELOW HERE!!
 
   end
 )
